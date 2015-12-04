6ec65ba7782c33cb7db2c09da95096c09aab5935
mfzhang412 <mfzhang@NN123-HA-07.HS.sd203.org>
2015-12-04 15:10:41 -0600

    Magic Squares Day 2

--- Chapter 7 Practice/MagicSquares.java
+++ Chapter 7 Practice/MagicSquares.java
@@ -13,25 +13,83 @@ import java.util.Scanner;
 public class MagicSquares
 {
     private int[][] matrix = new int[4][4];
+    private int[][] storage = new int[3][4];
+    private boolean[] yesNo = new boolean[10];
+    private int numberOfSame = 0;
     Scanner s = new Scanner(System.in);
     
     public void magicSqures()
     {
-        int counter = 1;
         System.out.println("Enter in the 16 numbers.");
         
-        for (int i = 0; i < 16; i++)
+        for (int row = 0; row < matrix.length; row++)
         {
-            System.out.print("Number " + counter + ": ");
-            for (int row = 0; row < matrix.length; row++)
+            for (int col = 0; col < matrix[row].length; col++)
             {
-                for (int col = 0; col < matrix[row]; col++)
+                
+                System.out.print("Number in row " + row + " and column " + col + ": ");
+                if ((s.nextInt() > 0) || (s.nextInt() < 17))
                 {
                     matrix[row][col] = s.nextInt();
                 }
-            }
-            counter++;
+                else
+                {
+                    do
+                    {
+                        System.out.println("Please enter a number 1-16: ");
+                    }
+                    while ((s.nextInt() < 1) || (s.nextInt() > 16));
+                    matrix[row][col] = s.nextInt();
+                }
+           }
         }
         
+        for (int row = 0; row < matrix.length; row++)
+        {
+            int total = 0;
+            for (int col = 0; col < matrix[row].length; col++)
+            {
+                total += matrix[row][col];
+            }
+            storage[0][row] = total;
+        }
+        
+        for (int row = 0; row < matrix.length; row++)
+        {
+            int total = 0;
+            for (int col; col < matrix[row].length; col++)
+            {
+                total += matrix[col][row];
+            }
+            storage[1][row] = total;
+        }
+        
+        for (int row = 0; row < matrix.length; row++)
+        {
+            int total += matrix[row][row];
+        }
+        storage[2][0] = total;
+        storage[2][1] = total; //to fill up the last matrix for the diagonal
+        
+        for (int row = matrix.length; row > 0; row--)
+        {
+            int total += matrix[row][row];
+        }
+        storage[2][2] = total;
+        storage[2][3] = total; // to fill up the last matrix for the diagonal
+        
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                for (int yesNoCounter = 0; yesNoCounter < 10; yesNoCounter++)
+                {
+                    if (matrix[i][j] == matrix[i][j+1])
+                    {
+                        yesNo[yesNoCounter] = true;
+                    }
+                }
+            }
+        }
     }
 }
